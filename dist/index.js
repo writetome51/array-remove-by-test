2db2152150d7e4c6e6dd5b1e58564a0eb288e16d
Steve Thompson <writetome51@gmail.com>
2020-09-09 14:53:19 -0600

    Upped version to 2.1.0

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,20 @@
-import { getAndRemoveByIndex } from '@writetome51/array-get-and-remove-by-index';
+import {getAndRemoveByIndex} from '@writetome51/array-get-and-remove-by-index';
+
+
 export function removeByTest(test, array, useRemovedItem = () => void 0) {
-    const action = (value, index, array) => {
-        let removed = getAndRemoveByIndex(index, array);
-        useRemovedItem(removed, index, array);
-    };
-    forEach_ifPasses_doAction_fromRight(test, action, array);
-    function forEach_ifPasses_doAction_fromRight(test, action, array) {
-        for (let i = array.length - 1; i > -1; --i) {
-            if (test(array[i], i, array))
-                action(array[i], i, array);
-        }
-    }
+
+	const action = (value, index, array) => {
+		let removed = getAndRemoveByIndex(index, array);
+		useRemovedItem(removed, index, array);
+	};
+	forEach_ifPasses_doAction_fromRight(test, action, array);
+
+
+	function forEach_ifPasses_doAction_fromRight(test, action, array) {
+
+		for (let i = array.length - 1; i > -1; --i) {
+			if (test(array[i], i, array)) action(array[i], i, array);
+		}
+	}
+
 }
-/***********
-
-export getAndRemoveByTest(
-    test: (value, index?, array?) => boolean,
-    array,
-    getValue: (value, index?, array?) => any = (value) => value,
-): any[] {
-
-    let found = [];
-
-    // Removes items in descending index order:
-    removeByTest(test, array, (value, index, array) => {
-        found.push(getValue(value, index, array));
-    });
-
-    // Return items in ascending index order:
-    return reverse(found);
-}
-
- *************/
--- lib/index.ts
+++ lib/index.ts
@@ -24,26 +24,3 @@ export function removeByTest(
 		}
 	}
 }
-
-
-
-/***********
-
-export getAndRemoveByTest(
-	test: (value, index?, array?) => boolean,
-	array,
-	getValue: (value, index?, array?) => any = (value) => value,
-): any[] {
-
-	let found = [];
-
-	// Removes items in descending index order:
-	removeByTest(test, array, (value, index, array) => {
-		found.push(getValue(value, index, array));
-	});
-
-	// Return items in ascending index order:
-	return reverse(found);
-}
-
- *************/
